a9e37ae768ede0db109cb74d14f7e40d3537b6cb
mweser <matthew.weser@gmail.com>
2014-03-25 15:19:50 -0700

    Folder organization

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cse220_lab4
+//
+//  Created by Matthew Weser on 3/25/14.
+//  Copyright (c) 2014 mweser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
